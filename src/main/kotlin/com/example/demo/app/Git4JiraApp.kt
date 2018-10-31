ad76053c89a32846a1210e86415949f36f55eaea
flywheelms <steven.d.stamps@disney.com>
2018-10-31 13:02:50 -0400

    Git4JiraApp

--- src/main/kotlin/com/example/demo/app/Git4JiraApp.kt
+++ src/main/kotlin/com/example/demo/app/Git4JiraApp.kt
@@ -3,4 +3,4 @@ package com.example.demo.app
 import com.example.demo.view.MainView
 import tornadofx.App
 
-class MyApp: App(MainView::class, Styles::class)
+class Git4JiraApp: App(MainView::class, Styles::class)
