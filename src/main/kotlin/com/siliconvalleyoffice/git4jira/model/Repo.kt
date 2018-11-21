8c18442300b64e479ab9b12db7403067330c10e7
flywheelms <steven.d.stamps@disney.com>
2018-11-21 16:54:16 -0500

    Repo:JsonModel

--- src/main/kotlin/com/siliconvalleyoffice/git4jira/model/Repo.kt
+++ src/main/kotlin/com/siliconvalleyoffice/git4jira/model/Repo.kt
@@ -1,6 +1,5 @@
 package com.siliconvalleyoffice.git4jira.model
 
-import javafx.beans.property.SimpleBooleanProperty
 import javafx.beans.property.SimpleStringProperty
 import tornadofx.*
 import javax.json.JsonObject
@@ -8,23 +7,30 @@ import javax.json.JsonObject
 class Repo : JsonModel {
     val nameProperty = SimpleStringProperty()
     var name by nameProperty
-    val ownerProperty = SimpleStringProperty()
-    var owner by ownerProperty
-    val isForkProperty = SimpleStringProperty()
-    var isFork by isForkProperty
-    val gitUrlProperty = SimpleStringProperty()
-    var git_url by gitUrlProperty
+    val descriptionProperty = SimpleStringProperty()
+    var description by descriptionProperty
+    val forkProperty = SimpleStringProperty()
+    var fork by forkProperty
     val cloneUrlProperty = SimpleStringProperty()
     var clone_url by cloneUrlProperty
+    val branchesUrlProperty = SimpleStringProperty()
+    var branchesUrl by branchesUrlProperty
+    val pullsUrlProperty = SimpleStringProperty()
+    var pullsUrl by pullsUrlProperty
+    val mergesUrlProperty = SimpleStringProperty()
+    var mergesUrl by mergesUrlProperty
     val defaultBranchProperty = SimpleStringProperty()
     var defaultBranch by defaultBranchProperty
 
     override fun updateModel(json: JsonObject) {
         with (json) {
             name = string("name")
-            isFork = string("fork")
-            git_url = string("git_url")
+            description = string("description")
+            fork = string("fork")
             clone_url = string("clone_url")
+            branchesUrl = string("branches_url")
+            pullsUrl = string("pulls_url")
+            mergesUrl = string("merges_url")
             defaultBranch = string("default_branch")
         }
     }
@@ -32,7 +38,11 @@ class Repo : JsonModel {
 
 class RepoModel : ItemViewModel<Repo>() {
     val name = bind(Repo::nameProperty)
-    val gitUrl = bind(Repo::gitUrlProperty)
+    val description = bind(Repo::descriptionProperty)
+    val fork = bind(Repo::forkProperty)
     val cloneUrl = bind(Repo::cloneUrlProperty)
+    val branchesUrl = bind(Repo::branchesUrlProperty)
+    val pullsUrl = bind(Repo::pullsUrlProperty)
+    val mergesUrl = bind(Repo::mergesUrlProperty)
     val defaultBranch = bind(Repo::defaultBranchProperty)
 }
