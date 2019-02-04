2de6fa10e5e13d70ab5869c678a693b0f8504cc5
Hardeep-Singh <hardeep.singh@espn.com>
2019-02-04 13:25:19 -0500

    Research

--- src/main/kotlin/com/siliconvalleyoffice/git4jira/app/Git4JiraApp.kt
+++ src/main/kotlin/com/siliconvalleyoffice/git4jira/app/Git4JiraApp.kt
@@ -3,8 +3,10 @@ package com.siliconvalleyoffice.git4jira.app
 import com.siliconvalleyoffice.git4jira.dagger.Injector
 import com.siliconvalleyoffice.git4jira.view.LoginView
 import com.siliconvalleyoffice.git4jira.view.LoginViewOld
+import javafx.fxml.FXMLLoader
 import javafx.scene.image.Image
 import javafx.stage.Stage
+import javafx.stage.StageStyle
 import tornadofx.*
 
 class Git4JiraApp: App(LoginViewOld::class, Styles::class) {
@@ -16,11 +18,12 @@ class Git4JiraApp: App(LoginViewOld::class, Styles::class) {
 
     override fun start(stage: Stage) {
         super.start(stage)
+        find<LoginView>().openModal(stageStyle = StageStyle.TRANSPARENT)
 
         //Setup Stage
         FX.primaryStage.icons += Image("https://avatars2.githubusercontent.com/u/10121039?v=4")
         stage.isResizable = true
-        stage.width = LOGIN_VIEW_WIDTH
-        stage.height = LOGIN_VIEW_HEIGHT
+//        stage.width = LOGIN_VIEW_WIDTH
+//        stage.height = LOGIN_VIEW_HEIGHT
     }
 }
--- src/main/kotlin/com/siliconvalleyoffice/git4jira/view/LoginView.kt
+++ src/main/kotlin/com/siliconvalleyoffice/git4jira/view/LoginView.kt
@@ -3,25 +3,126 @@ package com.siliconvalleyoffice.git4jira.view
 import com.siliconvalleyoffice.git4jira.contracts.Login
 import com.siliconvalleyoffice.git4jira.dagger.Injector
 import com.siliconvalleyoffice.git4jira.dagger.LoginModule
-import javafx.scene.layout.VBox
+import com.sun.prism.paint.Color
+import com.sun.prism.paint.Paint
+import javafx.geometry.Insets
+import javafx.geometry.Pos
+import javafx.scene.control.TextField
+import javafx.scene.layout.StackPane
+import javafx.scene.text.Text
 import tornadofx.*
 import javax.inject.Inject
 
-class LoginView: View(), Login.View {
+class LoginView : View(), Login.View {
 
     @Inject
     lateinit var loginController: Login.Controller
 
-    override val root = VBox()
+    override val root = StackPane()
 
     init {
         Injector.Instance.appComponent.plus(LoginModule(this)).inject(this)
-        title = "Github Login"
 
         with(root) {
-            text {
-                text = loginController.toString()
+            setPrefSize(700.0, 400.0)
+            style {
+                backgroundColor += c("#000000", 0.8)
             }
+            hbox(30) {
+                paddingAll = 40.0
+                vbox {
+                    prefWidth = 300.0
+                    paddingAll = 40.0
+                    style {
+                        backgroundColor += c("#FFFFFF", 1.0)
+                    }
+                    text {
+                        headerTextStyle(25.px)
+                        text = "GithHub"
+                        marginBottom(40.0)
+                    }
+                    textfield {
+                        textFieldStyle(15.px)
+                        promptText = "Username"
+                        marginBottom(20.0)
+                    }
+                    textfield {
+                        textFieldStyle(15.px)
+                        promptText = "Password"
+                        marginBottom(30.0)
+                    }
+                    add(createLogInButton())
+                }
+
+                //Right
+                vbox {
+                    prefWidth = 300.0
+                    paddingAll = 40.0
+                    style {
+                        backgroundColor += c("#FFFFFF", 1.0)
+                    }
+                    text {
+                        headerTextStyle(25.px)
+                        text = "JIRA"
+                        marginBottom(40.0)
+                    }
+                    textfield {
+                        border
+                        textFieldStyle(15.px)
+                        promptText = "Username"
+                        marginBottom(20.0)
+                    }
+                    textfield {
+                        textFieldStyle(15.px)
+                        promptText = "Password"
+                        marginBottom(30.0)
+                    }
+                    add(createLogInButton())
+                }
+            }
+        }
+    }
+
+    //Create Login Button
+    private fun createLogInButton() =
+        hbox {
+            alignment = Pos.BASELINE_RIGHT
+            button("Log In") {
+                style {
+                    fontSize = 17.px
+                    fontFamily = "Times New Roman"
+                    backgroundColor += c("#000000", 0.8)
+                    textFill = c("#FFFFFF", 1.0)
+                }
+            }
+        }
+
+    private fun Text.marginBottom(value: Double) {
+        vboxConstraints {
+            marginBottom = value
+        }
+    }
+
+    private fun TextField.marginBottom(value: Double) {
+        vboxConstraints {
+            marginBottom = value
+        }
+    }
+
+    private fun Text.headerTextStyle(fontDimension: Dimension<Dimension.LinearUnits>) {
+        style {
+            fontSize = fontDimension
+            fontFamily = "Times New Roman"
+        }
+    }
+
+    private fun TextField.textFieldStyle(fontDimension: Dimension<Dimension.LinearUnits>) {
+        style {
+            textBoxBorder = c("#00000000", 0.0)
+            borderWidth += CssBox(1.px, 0.px, 0.px, 0.px)
+            focusColor = c("#000000", 0.5)
+            fontSize = fontDimension
+            fontFamily = "Times New Roman"
         }
     }
 
