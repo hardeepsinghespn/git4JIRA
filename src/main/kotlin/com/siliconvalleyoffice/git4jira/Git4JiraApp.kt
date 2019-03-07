652df5f508e08b4a5a3e25d6d8bf24fcf568150b
Hardeep-Singh <hardeep.singh@espn.com>
2019-03-06 20:34:50 -0500

    Clean up

--- src/main/kotlin/com/siliconvalleyoffice/git4jira/Git4JiraApp.kt
+++ src/main/kotlin/com/siliconvalleyoffice/git4jira/Git4JiraApp.kt
@@ -1,11 +1,9 @@
 package com.siliconvalleyoffice.git4jira
 
-import com.siliconvalleyoffice.git4jira.service.git.GitAuthInterceptor
 import com.siliconvalleyoffice.git4jira.dagger.Injector
-import com.siliconvalleyoffice.git4jira.service.*
+import com.siliconvalleyoffice.git4jira.service.Service
 import com.siliconvalleyoffice.git4jira.view.HomeView
 import tornadofx.*
-import view.Git4JiraCredentialsView
 import javax.inject.Inject
 
 class Git4JiraApp : App(HomeView::class) {
